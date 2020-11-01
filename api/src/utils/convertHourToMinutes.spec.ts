234681c15d955f657a89a9250ee6a5d566914881
joilson <joilson_rg@hotmail.com>
2020-11-01 16:00:28 -0300

    chore: remove import

--- api/src/utils/convertHourToMinutes.spec.ts
+++ api/src/utils/convertHourToMinutes.spec.ts
@@ -1,5 +1,4 @@
 import convertHourToMinutes from './convertHourToMinutes';
-import InvalidTimeError from '../exceptions/InvalidTimeError';
 describe('convertHourToMinutes', () => {
     it('should return a valid time in minutes', () => {
         const minutes = convertHourToMinutes('08:00');
