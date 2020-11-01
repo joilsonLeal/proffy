19bc615776e87f89e7b53e44d5b4ef2431960846
joilson <joilson_rg@hotmail.com>
2020-11-01 15:59:41 -0300

    test: coverage convertHourToMinutes

--- api/src/utils/convertHourToMinutes.spec.ts
+++ api/src/utils/convertHourToMinutes.spec.ts
@@ -1,8 +1,12 @@
 import convertHourToMinutes from './convertHourToMinutes';
-
+import InvalidTimeError from '../exceptions/InvalidTimeError';
 describe('convertHourToMinutes', () => {
     it('should return a valid time in minutes', () => {
         const minutes = convertHourToMinutes('08:00');
         expect(minutes).toBe(480);
-    })
+    });
+
+    it('should throw an InvalidTimeError', () => {
+        expect(() => convertHourToMinutes('error')).toThrow();
+    });
 });
