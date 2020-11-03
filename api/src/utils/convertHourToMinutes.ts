6b59c2dbc03197b2833951eaca2cd47e009702df
joilson <joilson_rg@hotmail.com>
2020-11-03 08:11:15 -0300

    fix: time regex

--- api/src/utils/convertHourToMinutes.ts
+++ api/src/utils/convertHourToMinutes.ts
@@ -1,7 +1,7 @@
 import InvalidTimeError from '../exceptions/InvalidTimeError';
 
 export default function convertHourToMinutes(time: string) {
-        if(/^[0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}$/.test(time)) {
+        if(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/.test(time)) {
             const [ hour, minutes ] = time.split(':').map(Number);
             const timeInMinutes = (hour * 60) + minutes;
             return timeInMinutes;
